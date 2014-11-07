fb4ddfa7a463da51a8f1470a245b0964ab351b2e
Dirkpeters04 <dirkpeters94@web.de>
2014-11-07 16:53:39 +0100

    Hier noch die Sender-Datei mit demselben Problem

---
+++ Aufgabe3/Sender.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+
+int main (){
+
+	int df,err;
+	struct sockaddr_in addr;
+
+	//Socket initialisieren
+	df = socket(AF_INET,SOCK_DGRAM,0);
+	if (df<0) { 
+		printf ("Achtung bei der Funktion Socket ist ein Fehler aufgetreten\n");
+	}
+	
+	//ÜBergabe der Adressen-Parameter Bei Port 0 für vom System aussuchen
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(0);
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	printf ("Wenn sie aus der Schleife wollen geben sie exit als nächste zu sendende Nachricht ein");
+	char* run= (malloc(100*sizeof(char)));
+	char* exit;
+	exit = "exit";
+	
+	char msg[64];
+	
+	struct sockaddr_in dest;
+	
+	dest.sin_family = AF_INET;
+	dest.sin_port = htons(4711);
+	dest.sin_addr.s_addr = inet_addr("130.37.193.13");
+	
+	while (strcmp(run, exit) != 0){
+		strcpy(msg, run);
+		printf ("Geben sie die zu sendende Nachricht ein: \n");
+		scanf ("%s", run);	
+		err= sendto(df, msg, strlen(msg)+1,0, (struct sockaddr*) &dest,sizeof(struct sockaddr_in) );
+		if (err <0) {
+			printf (" Bei der Versendung der Datei ist ein Fehler aufgetreten");
+			run ="exit";
+		}
+	}	
+	
+	int err1;
+	err1= close (df);
+	if (err1<0)printf ("Bei dem Schließen des Sockets ist ein Fehler aufgetreten");
+	
+	return 0;
+}
