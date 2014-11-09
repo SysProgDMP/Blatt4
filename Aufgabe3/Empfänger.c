a5f3ece545772583b11fdad296c179ae84f999a6
Dirkpeters04 <dirkpeters94@web.de>
2014-11-09 11:47:57 +0100

    So jetzt ohne Fehler , aber so wie sie ist funktioniert sie noch nicht ganz dafür müsste ich noch die richtig ip Adresse einfügen

--- Aufgabe3/Empfänger.c
+++ Aufgabe3/Empfänger.c
@@ -29,12 +29,14 @@ int main(){
 		printf ("in der Funktion bind ist ein Fehler aufgetreten");
 	}
 	
+	int  lange= sizeof (struct sockaddr_in);
+	
 	//Ausgabe des Socketname
-	int socketname=getsockname(fd, (struct sockaddr *) &addr, sizeof(struct sockaddr_in));
+	int socketname=getsockname(fd, (struct sockaddr *) &addr, &lange);
 	printf ("Das ist die Portnummer von dem aktuellen Prozess:  %i",socketname);
 	
 	// Variablen für recvfrom
-	char msg[64];
+	char* msg = malloc (100* sizeof (char));
 	int len, flen;
 	struct sockaddr_in from;
 	flen = sizeof(struct sockaddr_in);
@@ -49,13 +51,14 @@ int main(){
 	//Ausgabe der empfangenen Daten
 	int i;
 	for (i=0; i<64;i++){
-		printf ("%s  ", msg[i]);
+		printf ("%i  ", *(msg+i));
 	}
 	
 	int err1;
 	err1= close (fd);
 	if (err1<0)printf ("Bei dem Schließen des Sockets ist ein Fehler aufgetreten");
-	
+	 
+	free (msg);
 	
 	return 0;	
 }
--- Aufgabe3/Sender.c
+++ Aufgabe3/Sender.c
@@ -24,6 +24,7 @@ int main (){
 	addr.sin_port = htons(0);
 	addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	
+
 	printf ("Wenn sie aus der Schleife wollen geben sie exit als nächste zu sendende Nachricht ein");
 	char* run= (malloc(100*sizeof(char)));
 	char* exit;
@@ -47,10 +48,11 @@ int main (){
 			run ="exit";
 		}
 	}	
-	
+	free (run);
 	int err1;
 	err1= close (df);
 	if (err1<0)printf ("Bei dem Schließen des Sockets ist ein Fehler aufgetreten");
 	
 	return 0;
 }
+	
